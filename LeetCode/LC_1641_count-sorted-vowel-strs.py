8cfcec4eca818371cf66bc3080da689f7233ffa4
JHJee <chlwlgp132@gmail.com>
2022-05-11 15:41:26 +0900

    220511

--- LeetCode/LC_1641_count-sorted-vowel-strs.py
+++ LeetCode/LC_1641_count-sorted-vowel-strs.py
@@ -1,16 +1,8 @@
-# 1641. Count Sorted Vowel Strings
-
-https://leetcode.com/problems/count-sorted-vowel-strings/
-
-```python
 import math
+
 
 class Solution:
     def countVowelStrings(self, n: int) -> int:
         r = n
         n = 4 + r
         return int(math.factorial(n) / (math.factorial(n - r) * math.factorial(r)))
-```
-
-- Combinations with repetitions
-- Return nHr
